34c80ddebbdfe3f6a1c16ac1652be385b533374c
shay213 <dawidpawelec@o2.pl>
2023-11-19 12:09:59 +0100

    Create basic explosion class, and draw rects on click

---
+++ js/Explosion.js
@@ -0,0 +1,28 @@
+export default class Explosion{
+  constructor(x, y){
+    this.x = x
+    this.y = y
+
+    this.spriteWidth = 200
+    this.spriteHeight = 179
+    this.width = this.spriteWidth * 0.5
+    this.height = this.spriteHeight * 0.5
+
+    this.img = new Image()
+    this.img.src = '../assets/boom.png'
+
+    this.frame = 0
+    this.gameFrame = 0
+  }
+
+  update(){
+    this.gameFrame++
+    if(this.gameFrame % 5 === 0){
+      this.frame < 5 ? this.frame++ : this.frame = 0
+    }
+  }
+
+  draw(ctx){
+    ctx.drawImage(this.img, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+import Explosion from "./Explosion.js"
+
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 const CANVAS_WIDTH = canvas.width = 500
 const CANVAS_HEIGHT = canvas.height = 700
 
-ctx.fillStyle = 'white'
-ctx.fillRect(50, 50, 100, 100)
+const canvasPos = canvas.getBoundingClientRect()
+
+window.addEventListener('click', (e) => {
+  ctx.fillStyle = 'white'
+  ctx.fillRect(e.x - canvasPos.left, e.y - canvasPos.top, 20, 20)
+})
