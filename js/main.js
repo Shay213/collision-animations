8b8d0378c4c37bfa2e60246cfc2bd4998893d141
shay213 <dawidpawelec@o2.pl>
2023-11-19 11:50:40 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const CANVAS_WIDTH = canvas.width = 500
+const CANVAS_HEIGHT = canvas.height = 700
+
+ctx.fillStyle = 'white'
+ctx.fillRect(50, 50, 100, 100)
